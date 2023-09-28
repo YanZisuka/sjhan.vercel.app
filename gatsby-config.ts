813efb961c5a2376e5a76ed14c79399056dc8796
Kirby <sj.han@modusign.co.kr>
2023-09-28 12:42:04 +0900

    fix: gatsby.config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,5 +1,5 @@
-import type { GatsbyConfig, PluginRef } from "gatsby"
-import "dotenv/config"
+import type { GatsbyConfig, PluginRef } from 'gatsby'
+import 'dotenv/config'
 
 const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
@@ -8,14 +8,14 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.mjs
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://minimal-blog.lekoarts.de`,
-    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
-    siteImage: `/banner.jpg`,
-    siteLanguage: `en`,
-    author: `@lekoarts_de`,
+    siteTitle: `Kirby`,
+    siteTitleAlt: `Kirby`,
+    siteHeadline: `Kirby's Blog`,
+    siteUrl: `https://sjhan.netlify.app`,
+    siteDescription: `프론트엔드 개발자 한승재 블로그`,
+    siteImage: `/banner.png`,
+    siteLanguage: `ko`,
+    author: `@YanZisuka`,
   },
   trailingSlash: `never`,
   plugins: [
@@ -35,12 +35,8 @@ const config: GatsbyConfig = {
         ],
         externalLinks: [
           {
-            name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
-          },
-          {
-            name: `Homepage`,
-            url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
+            name: `Github`,
+            url: `https://github.com/YanZisuka`,
           },
         ],
       },
@@ -54,9 +50,9 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `Kirby's Blog`,
+        short_name: `Kirby`,
+        description: `프론트엔드 개발자 한승재 블로그`,
         start_url: `/`,
         background_color: `#fff`,
         // This will impact how browsers show your PWA/website
@@ -97,9 +93,12 @@ const config: GatsbyConfig = {
             serialize: ({
               query: { site, allPost },
             }: {
-              query: { allPost: IAllPost; site: { siteMetadata: ISiteMetadata } }
+              query: {
+                allPost: IAllPost
+                site: { siteMetadata: ISiteMetadata }
+              }
             }) =>
-              allPost.nodes.map((post) => {
+              allPost.nodes.map(post => {
                 const url = site.siteMetadata.siteUrl + post.slug
                 const content = `<p>${post.excerpt}</p><div style="margin-top: 50px; font-style: italic;"><strong><a href="${url}">Keep reading</a>.</strong></div><br /> <br />`
 
@@ -109,7 +108,7 @@ const config: GatsbyConfig = {
                   excerpt: post.excerpt,
                   url,
                   guid: url,
-                  custom_elements: [{ "content:encoded": content }],
+                  custom_elements: [{ 'content:encoded': content }],
                 }
               }),
             query: `{
@@ -123,7 +122,7 @@ const config: GatsbyConfig = {
   }
 }`,
             output: `rss.xml`,
-            title: `Minimal Blog - @lekoarts/gatsby-theme-minimal-blog`,
+            title: `Kirby's Blog`,
           },
         ],
       },

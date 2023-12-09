25953aa65bf76671d60a6ff7debc8b4e98ad5c31
Kirby <sj.han@modusign.co.kr>
2023-12-09 15:31:04 +0900

    fix: 모드 전환 버그

--- src/gatsby-plugin-theme-ui/index.ts
+++ src/gatsby-plugin-theme-ui/index.ts
@@ -1,7 +1,7 @@
 import baseTheme from '@lekoarts/gatsby-theme-minimal-blog/src/gatsby-plugin-theme-ui'
+import { merge } from 'theme-ui'
 
-const theme = {
-  ...baseTheme,
+const theme = merge(baseTheme, {
   fontSizes: [
     '0.875rem',
     '1rem',
@@ -15,18 +15,15 @@ const theme = {
     '4.5rem',
   ],
   fonts: {
-    ...baseTheme.fonts,
     body: '"Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"',
   },
   text: {
-    ...baseTheme.text,
     default: {
       color: 'text',
       fontSize: [1, 1, 2],
     },
   },
   badges: {
-    ...baseTheme.badges,
     outline: {
       color: 'primary',
       bg: 'transparent',
@@ -36,19 +33,18 @@ const theme = {
     },
   },
   space: [
-    baseTheme.space?.[0],
-    baseTheme.space?.[1],
-    baseTheme.space?.[2],
-    baseTheme.space?.[3],
-    baseTheme.space?.[4],
-    baseTheme.space?.[5],
-    baseTheme.space?.[6],
-    baseTheme.space?.[6],
-    baseTheme.space?.[6],
+    baseTheme.space![0],
+    baseTheme.space![1],
+    baseTheme.space![2],
+    baseTheme.space![3],
+    baseTheme.space![4],
+    baseTheme.space![5],
+    baseTheme.space![6],
+    baseTheme.space![6],
+    baseTheme.space![6],
   ],
   useColorSchemeMediaQuery: true,
   alerts: {
-    ...baseTheme.alerts,
     highlight: {
       color: 'background',
       bg: 'primary',
@@ -58,7 +54,6 @@ const theme = {
     },
   },
   colors: {
-    ...baseTheme.colors,
     backgroundSecondary: 'var(--theme-ui-colors-gray-2)',
     backgroundTertiary: 'var(--theme-ui-colors-gray-3)',
     twitterBlue: '#1d9bf0',
@@ -68,13 +63,7 @@ const theme = {
     danger: 'var(--theme-ui-colors-red-6)',
     warning: 'var(--theme-ui-colors-yellow-7)',
     modes: {
-      ...baseTheme.colors?.modes,
-      light: {
-        ...baseTheme.colors?.modes?.light,
-        // background: '#F5F5F5',
-      },
       dark: {
-        ...baseTheme.colors?.modes?.dark,
         danger: 'var(--theme-ui-colors-red-7)',
         twitterPollBg: 'rgb(51, 54, 57)',
         backgroundSecondary: 'var(--theme-ui-colors-gray-8)',
@@ -84,32 +73,25 @@ const theme = {
     },
   },
   styles: {
-    ...baseTheme.styles,
     h1: {
-      ...baseTheme.styles?.h1,
       overflowWrap: 'break-word',
     },
     h2: {
-      ...baseTheme.styles?.h2,
       overflowWrap: 'break-word',
     },
     h3: {
-      ...baseTheme.styles?.h3,
       overflowWrap: 'break-word',
     },
     h4: {
-      ...baseTheme.styles?.h4,
       overflowWrap: 'break-word',
     },
     h5: {
-      ...baseTheme.styles?.h5,
       overflowWrap: 'break-word',
     },
     h6: {
-      ...baseTheme.styles?.h6,
       overflowWrap: 'break-word',
     },
   },
-}
+})
 
 export default theme

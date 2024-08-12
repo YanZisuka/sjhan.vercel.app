ae5438876df3338dc1fb70cc6c7f7db8930a1143
YanZisuka <hanndrednine@gmail.com>
2024-08-13 02:23:44 +0900

    feat: add @vercel/speed-insights

--- src/@lekoarts/gatsby-theme-minimal-blog/components/layout.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/layout.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import OriginalLayout from '@lekoarts/gatsby-theme-minimal-blog/src/components/layout'
+import { useLocation } from '@reach/router'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 interface Props extends React.ComponentProps<typeof OriginalLayout> {}
 
@@ -9,6 +11,13 @@ export default function Layout(props: Props) {
     <>
       <OriginalLayout {...props} />
       <Analytics />
+      <Insights />
     </>
   )
+}
+
+function Insights() {
+  const location = useLocation()
+
+  return <SpeedInsights route={location.pathname} />
 }
